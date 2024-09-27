81743c6f2cbd74ce4bd2fa67d0ef0ef83f04f050
Andy Vaughan <20927751+floodmeadows@users.noreply.github.com>
2024-09-27 21:17:27 +0100

    Add in buttons for half day (3h34m).

--- fireglass.user.js
+++ fireglass.user.js
@@ -4,7 +4,7 @@
 // @description  Adds shortcut buttons to Fieldglass to make it quicker and easier to fill in your timesheets
 // @copyright    2020, floodmeadows (https://openuserjs.org/users/floodmeadows)
 // @license      MIT
-// @version      0.4.0
+// @version      1.0
 // @include      https://www.fieldglass.net/rate_schedule_time_sheet_form.do*
 // @grant        none
 // ==/UserScript==
@@ -19,6 +19,8 @@
     // Config variables
     var fullDayHours = 7;
     var fullDayMins = 30;
+    var halfDayHours = 3;
+    var halfDayMins = 45;
     var buttonStyle = "margin: 0px 2px 4px; padding: 0px 2px; min-width: 30%; height: 22px; background-color: #ccf; border: 0px;";
     var weekButtonStyle = " width: 80%; margin: 4px auto;";
     var dayButtonsContainerStyle = "display:inline-block; width:2.4em; vertical-align:top;";
@@ -27,6 +29,7 @@
     var fullWeekText = fullDayHours + ":" + fullDayMins + " every working day";
     var nonWeekText = "0:00 every day";
     var noneDayText = "0:00";
+    var halfDayText = halfDayHours + ":" + halfDayMins;
     var fullDayText = fullDayHours + ":" + fullDayMins;
 
     // add buttons to set values for the whole week
@@ -66,11 +69,16 @@
     fullDayButton.innerHTML = fullDayText;
     fullDayButton.setAttribute('style', buttonStyle);
 
+    var halfDayButton = document.createElement('button');
+    halfDayButton.innerHTML = halfDayText;
+    halfDayButton.setAttribute('style', buttonStyle);
+
     var noneDayButton = document.createElement('button');
     noneDayButton.innerHTML = noneDayText;
     noneDayButton.setAttribute('style', buttonStyle);
 
     dayButtons.appendChild(fullDayButton);
+    dayButtons.appendChild(halfDayButton);
     dayButtons.appendChild(noneDayButton);
 
     document.querySelectorAll("td.hoursWorked").forEach(
@@ -111,6 +119,8 @@
                     clearAllDays(element);
                 } else if(element.innerHTML == fullDayText) {
                     fillDay(element, fullDayHours, fullDayMins);
+                } else if(element.innerHTML == halfDayText) {
+                    fillDay(element, halfDayHours, halfDayMins);
                 } else if(element.innerHTML == noneDayText) {
                     fillDay(element, 0, 0);
                 }
